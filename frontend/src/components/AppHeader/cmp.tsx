b6594e676361aa5a4db063186c2557034cfc0cb3
gmolki <gerardmo98@gmail.com>
2025-02-25 14:55:41 -0700

    feat: deploy agent form

--- frontend/src/components/AppHeader/cmp.tsx
+++ frontend/src/components/AppHeader/cmp.tsx
@@ -8,10 +8,7 @@ export default function AppHeader() {
 
   return (
     <section className="w-full px-6 pt-4 mb-4 border-b-2 h-fit">
-      <div
-        className="flex items-center justify-between w-full px-2 mb-4 gap-3 h-20 cursor-pointer"
-        onClick={() => navigationRouter.push("/")}
-      >
+      <div className="flex items-center justify-between w-full px-2 mb-4 gap-3 h-20">
         {/* <Image
           src="/logo.svg"
           alt="Logo"
@@ -19,7 +16,10 @@ export default function AppHeader() {
           height={0}
           className="h-full w-auto"
         /> */}
-        <div className="flex flex-col h-full justify-around">
+        <div
+          className="flex flex-col h-full justify-around cursor-pointer"
+          onClick={() => navigationRouter.push("/")}
+        >
           <h1 className="text-6xl text-primary font-accent">CreAItors</h1>
           <h2 className="text-base">Autonomous AI Agents</h2>
         </div>
--- frontend/src/pages/deploy/[agentId]/index.tsx
+++ frontend/src/pages/deploy/[agentId]/index.tsx
@@ -1,8 +1,31 @@
 import AgentDetails from "@/components/AgentDetails";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import useFetchAgents from "@/hooks/useFetchAgents";
+import { AppState } from "@/store/store";
 import { useRouter } from "next/router";
 import { useMemo } from "react";
+import { useForm } from "react-hook-form";
+import { useSelector } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import { useSignMessage } from "wagmi";
+
+export type ConfigureAgentDeployFormValues = {
+  name?: string;
+  agentId?: string;
+  agentHash?: string;
+  owner?: string;
+};
 
 export default function DeployAgentPage() {
   const router = useRouter();
@@ -10,16 +33,53 @@ export default function DeployAgentPage() {
     query: { agentId },
   } = router;
 
-  const { agents, loading } = useFetchAgents();
+  const { agents, isLoading: isLoadingAgents } = useFetchAgents();
+
+  const { alephAccount } = useSelector((state: AppState) => state.aleph);
 
   const agent = useMemo(
     () => agents.find((agent) => agent.id === agentId),
     [agents, agentId]
   );
 
+  const defaultValues: ConfigureAgentDeployFormValues = useMemo(() => {
+    return {
+      name: undefined,
+      agentId: uuidv4(),
+      agentHash: agent?.id,
+      owner: alephAccount?.address,
+    };
+  }, [agent, alephAccount]);
+
+  const form = useForm({
+    defaultValues,
+  });
+
+  const { handleSubmit, control } = form;
+
+  const { signMessageAsync } = useSignMessage();
+
+  const onSubmit = async (data: ConfigureAgentDeployFormValues) => {
+    const unsignedAgentKey = `SIGN AGENT ${data.owner} ${data.agentId}`;
+    const signedAgentKey = await signMessageAsync({
+      message: unsignedAgentKey,
+    });
+
+    const requestBody = {
+      name: data.name,
+      agent_id: data.agentId,
+      agent_hash: data.agentHash,
+      owner: data.owner,
+      agent_key: signedAgentKey,
+    };
+    console.log("requestBody", requestBody);
+  };
+
+  if (isLoadingAgents) return <div>Loading...</div>;
   if (!agent) return <div>Agent not found</div>;
+
   return (
-    <div className="flex gap-8 mx-8">
+    <div className="flex gap-8 mx-8 mt-2">
       <div className="flex max-w-[33%]">
         <div className="flex flex-col gap-4 ">
           <p className="font-extrabold text-5xl">Selected Agent</p>
@@ -29,8 +89,31 @@ export default function DeployAgentPage() {
       </div>
       <div className="h-full"></div>
       <div className="flex flex-col gap-4 max-w-[66%]">
-        <p className="font-extrabold text-5xl">Deploy Agent</p>
-        <div>Deploy form</div>
+        <p className="font-extrabold text-5xl">Configure Agent Deploy</p>
+        <Form {...form}>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-4"
+          >
+            <FormField
+              control={control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormMessage />
+                  <FormControl>
+                    <Input placeholder="My AI Agent" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Used to easily identify your running agent
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </div>
     </div>
   );
