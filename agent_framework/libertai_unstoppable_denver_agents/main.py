57a383ec9fcbf6d2d2c319286ad9dce36b304427
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-02-24 20:02:58 -0700

    feat(framework): Working additional AgentKit tools

--- agent_framework/libertai_unstoppable_denver_agents/main.py
+++ agent_framework/libertai_unstoppable_denver_agents/main.py
@@ -20,34 +20,6 @@ from .interfaces import AutonomousAgentConfig, ChatAgentArgs
 from .provider import aleph_convertion_action_provider
 from .utils import get_provider_langchain_tools
 
-load_dotenv()
-
-private_key = os.getenv("WALLET_PRIVATE_KEY", None)
-assert private_key is not None, "You must set PRIVATE_KEY environment variable"
-assert private_key.startswith("0x"), "Private key must start with 0x hex prefix"
-
-# Create Ethereum account from private key
-account = Account.from_key(private_key)
-
-# Initialize Ethereum Account Wallet Provider
-wallet_provider = EthAccountWalletProvider(
-    config=EthAccountWalletProviderConfig(account=account, chain_id="8453")
-)
-
-# Initialize AgentKit
-agentkit = AgentKit(
-    AgentKitConfig(
-        wallet_provider=wallet_provider,
-        action_providers=[
-            aleph_convertion_action_provider(),
-            erc20_action_provider(),
-            wallet_action_provider(),
-        ],
-    )
-)
-
-tools = get_langchain_tools(agentkit)
-
 
 class AutonomousAgent:
     agent: ChatAgent
@@ -55,6 +27,38 @@ class AutonomousAgent:
     def __init__(
         self, autonomous_config: AutonomousAgentConfig, **kwargs: Unpack[ChatAgentArgs]
     ):
+        load_dotenv()
+
+        private_key = os.getenv("WALLET_PRIVATE_KEY", None)
+        assert private_key is not None, "You must set PRIVATE_KEY environment variable"
+        assert private_key.startswith("0x"), "Private key must start with 0x hex prefix"
+
+        # Create Ethereum account from private key
+        account = Account.from_key(private_key)
+
+        # Initialize Ethereum Account Wallet Provider
+        wallet_provider = EthAccountWalletProvider(
+            config=EthAccountWalletProviderConfig(account=account, chain_id="8453")
+        )
+
+        # Initialize AgentKit
+        agentkit = AgentKit(
+            AgentKitConfig(
+                wallet_provider=wallet_provider,
+                action_providers=[
+                    aleph_convertion_action_provider(),
+                    erc20_action_provider(),
+                    wallet_action_provider(),
+                ],
+            )
+        )
+
+        agentkit_tools = get_langchain_tools(agentkit)
+
+        if kwargs["tools"] is None:
+            kwargs["tools"] = []
+        kwargs["tools"].extend(agentkit_tools)  # type: ignore
+
         existing_action_providers = [
             action_provider.name for action_provider in agentkit.action_providers
         ]
@@ -66,8 +70,6 @@ class AutonomousAgent:
                 raise ValueError(
                     f"The AgentKit action provider '{ak_action_provider.name}' is already present for the autonomous agent behavior, please remove it."
                 )
-            if kwargs["tools"] is None:
-                kwargs["tools"] = []
             kwargs["tools"].extend(  # type: ignore
                 [
                     Tool.from_langchain(t)
