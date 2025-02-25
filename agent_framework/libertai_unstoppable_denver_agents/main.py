e742316a4b6bbc84fd8b6eb52ace4460fb734ac5
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-02-24 19:56:52 -0700

    feat(framework): Started abstracting the autonomous agent creation

--- agent_framework/libertai_unstoppable_denver_agents/main.py
+++ agent_framework/libertai_unstoppable_denver_agents/main.py
@@ -7,7 +7,6 @@ from coinbase_agentkit import (
     EthAccountWalletProviderConfig,
     erc20_action_provider,
     wallet_action_provider,
-    weth_action_provider,
 )
 from coinbase_agentkit_langchain import get_langchain_tools
 from dotenv import load_dotenv
@@ -15,8 +14,11 @@ from eth_account import Account
 from libertai_agents.agents import ChatAgent
 from libertai_agents.interfaces.tools import Tool
 from libertai_agents.models import get_model
+from typing_extensions import Unpack
 
+from .interfaces import AutonomousAgentConfig, ChatAgentArgs
 from .provider import aleph_convertion_action_provider
+from .utils import get_provider_langchain_tools
 
 load_dotenv()
 
@@ -40,19 +42,49 @@ agentkit = AgentKit(
             aleph_convertion_action_provider(),
             erc20_action_provider(),
             wallet_action_provider(),
-            weth_action_provider(),
         ],
     )
 )
 
-
 tools = get_langchain_tools(agentkit)
 
-agent = ChatAgent(
+
+class AutonomousAgent:
+    agent: ChatAgent
+
+    def __init__(
+        self, autonomous_config: AutonomousAgentConfig, **kwargs: Unpack[ChatAgentArgs]
+    ):
+        existing_action_providers = [
+            action_provider.name for action_provider in agentkit.action_providers
+        ]
+
+        for (
+            ak_action_provider
+        ) in autonomous_config.agentkit_additional_action_providers:
+            if ak_action_provider.name in existing_action_providers:
+                raise ValueError(
+                    f"The AgentKit action provider '{ak_action_provider.name}' is already present for the autonomous agent behavior, please remove it."
+                )
+            if kwargs["tools"] is None:
+                kwargs["tools"] = []
+            kwargs["tools"].extend(  # type: ignore
+                [
+                    Tool.from_langchain(t)
+                    for t in get_provider_langchain_tools(
+                        ak_action_provider, agentkit.wallet_provider
+                    )
+                ]
+            )
+        self.agent = ChatAgent(**kwargs)
+
+
+autonomous_agent = AutonomousAgent(
+    autonomous_config=AutonomousAgentConfig(agentkit_additional_action_providers=[]),
     model=get_model("NousResearch/Hermes-3-Llama-3.1-8B"),
     system_prompt="You are a helpful agent that can interact onchain using an Ethereum Account Wallet. You have tools to send transactions, query blockchain data, and interact with contracts. If you run into a 5XX (internal) error, ask the user to try again later.",
-    tools=[Tool.from_langchain(t) for t in tools],
+    tools=[],
     expose_api=True,
 )
 
-app = agent.app
+app = autonomous_agent.agent.app
