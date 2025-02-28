98afb0b9f5746bdfb3a55891380d56d6998fceb2
gmolki <gerardmo98@gmail.com>
2025-02-27 19:06:12 -0700

    refactor: update backend api server

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const alephApiServer = "https://api2.aleph.im";
-export const agentsApiServer = "http://[2a01:4f8:110:142e:3:4742:df62:ed71]:8000";
+export const agentsApiServer = "https://creaitors-backend.andresmolins.dev";
 
 export const agentCategories = [
 	"Finance",
