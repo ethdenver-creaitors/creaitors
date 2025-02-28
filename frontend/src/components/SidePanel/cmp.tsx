d0e673fd9c843dd80f013ce89621e8479a1b39e4
gmolki <gerardmo98@gmail.com>
2025-02-27 19:04:45 -0700

    fix: side panel header bottom padding

--- frontend/src/components/SidePanel/cmp.tsx
+++ frontend/src/components/SidePanel/cmp.tsx
@@ -3,7 +3,6 @@ import { StyledBackdrop, StyledSidePanel } from "./styles";
 import { SidePanelProps } from "./types";
 import { ChevronDown, ChevronRight } from "lucide-react";
 import { Button } from "../ui/button";
-import { Separator } from "../ui/separator";
 import useBreakpoints from "@/hooks/breakpoints/useBreakpoints";
 
 export const SidePanel = ({ children, title, isOpen, onClose: handleClose }: SidePanelProps) => {
@@ -13,12 +12,11 @@ export const SidePanel = ({ children, title, isOpen, onClose: handleClose }: Sid
 		<>
 			<StyledBackdrop $isOpen={isOpen} onClick={handleClose} />
 			<StyledSidePanel $isOpen={isOpen}>
-				<div className="sticky top-0 bg-background pt-10 shadow-[0_0_10px_hsl(var(--foreground))]">
+				<div className="sticky top-0 bg-background pt-10 pb-4 shadow-[0_0_10px_hsl(var(--foreground))]">
 					<Button variant="ghost" onClick={handleClose} className="absolute top-4 left-4">
 						{isDesktop ? <ChevronRight /> : <ChevronDown />}
 					</Button>
 					<p className="flex justify-center items-center text-center w-full px-4 mb-4 font-bold text-3xl">{title}</p>
-					<Separator />
 				</div>
 				<div className="p-12">{children}</div>
 			</StyledSidePanel>
